d25f174aaa746033fe254a250f6e506c574dde44
Aaa <166749133+pawgchamp@users.noreply.github.com>
2025-05-20 21:31:38 +0100

    deploy

---
+++ js/application.js
@@ -0,0 +1,85 @@
+function getDateToday(){
+    let now   = new Date();
+    let year  = now.getFullYear();
+    let month = String(now.getMonth() + 1).padStart(2, "0");
+    let day   = String(now.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+}
+
+async function loadData() {
+    const content = document.querySelector( "#content" );
+
+    try {
+        const [tweetsJson, daysJson] = await Promise.all([
+            fetch("js/tweets.json"),
+            fetch("js/days.json"),
+        ]);
+        let tweets = await tweetsJson.json();
+        let days   = await daysJson.json();
+
+        let today = getDateToday();
+        let pairs = days[today] || [];
+
+        console.log( pairs );
+
+        let tweetMap = {};
+        tweets.forEach( (tweet) => {
+            tweetMap[tweet.id] = tweet;
+        });
+
+        let number = 1;
+
+        let html = "";
+        pairs.forEach( ( pair ) => {
+            const [id1, id2] = pair;
+            const tweet1 = tweetMap[id1];
+            const tweet2 = tweetMap[id2];
+
+            if( tweet1.rating > tweet2.rating ){
+                html += `<li class="pair winner-1">`;
+            } else {
+                html += `<li class="pair winner-2">`;
+            }
+
+            [tweet1, tweet2].forEach((tweet) => {
+                if( tweet ){
+                    let img    = tweet.images[0];
+                    let name   = tweet.short_desc;
+                    let rating = tweet.rating + '%';
+
+                    let priceFormatter = new Intl.NumberFormat('en-GB', {
+                        style: 'currency',
+                        currency: 'GBP',
+                    });
+                    let price = priceFormatter.format( tweet.price );
+
+                    html += `
+                    <figure>
+                        <img src="${img}" alt="${name}" />
+                        <figcaption>
+                            <p>${number}. ${name}: <strong>${price}</strong></p>
+                            <p class="rating">${rating}</p>
+                        </figcaption>
+                    </figure>
+                    `;
+                }
+            });
+            html += `</li>`;
+
+            number++;
+        });
+
+        content.innerHTML = html;
+    } catch (err) {
+        content.innerHTML = "<p>❌ Error! Beansed it up, couldn't get either the FootyScran tweets in totality or the ones you were meant to see today. Soz.</p>";
+        console.error(err);
+    }
+}
+
+loadData();
+
+
+
+document.querySelector( '.spoiler-checkbox' ).addEventListener( 'click', function(){
+    document.body.classList.toggle( 'spoiler' );
+});
